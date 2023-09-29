69b39f759a0484a6f9937c5324190f7923afa086
soumyarajbag <soumyarajbag@gmail.com>
2023-09-30 03:35:15 +0530

    Signin Feature Added

--- src/components/authentication/Signup.jsx
+++ src/components/authentication/Signup.jsx
@@ -6,19 +6,128 @@ import {
   InputGroup,
   InputRightElement,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
+  const toast = useToast();
   const [show, setShow] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [pic, setPic] = useState("");
+  const [loading, setLoading] = useState(false);
+  const history = useNavigate();
+  const postDetails = (pics) => {
+    setLoading(true);
+    if (pics === undefined) {
+      toast({
+        title: "Upload Image !",
+        description: "No Image Added .",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+      return;
+    }
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "chat-app");
+      data.append("cloud_name", "daqe7nvzn");
+      fetch("https://api.cloudinary.com/v1_1/daqe7nvzn/image/upload", {
+        method: "post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPic(data.url.toString());
+          console.log(data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setLoading(false);
+        });
+    } else {
+      toast({
+        title: "Upload Image !",
+        description: "Please Upload Image in jpg or png format.",
+        status: "error",
+        duration: 5000,
+        position: "top",
+        isClosable: true,
+      });
+      setLoading(false);
+    }
+  };
+  const submitHandler = async () => {
+    setLoading(true);
+    if (!name || !email || !password || !confirmPassword) {
+      toast({
+        title: "Empty Fields !",
+        description: "Please Fill All Fields.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+      setLoading(false);
+      return;
+    }
+    if (password !== confirmPassword) {
+      toast({
+        title: "Password Mismatch !",
+        description: "Please Check Your Password.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+      setLoading(false);
+      return;
+    }
 
-  const postDetails = () => {};
-  const submitHandler = () => {};
+    try {
+      const config = {
+        Headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        "/api/user",
+        { name, email, password, pic },
+        config
+      );
+      toast({
+        title: "Account Created !",
+        description: "Your Account is Created Successfully.",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      history.push("/chat");
+      return;
+    } catch (error) {
+      toast({
+        title: "Error !",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+      });
+      setLoading(false);
+      return;
+    }
+  };
   return (
     <VStack spacing={"5px"}>
       <FormControl isRequired>
@@ -99,7 +208,7 @@ const Signup = () => {
           type="file"
           p={1.5}
           accept="image/*"
-          value={pic}
+          value={pic.name}
           onChange={(e) => {
             postDetails(e.target.files[0]);
           }}
@@ -111,6 +220,7 @@ const Signup = () => {
         width={"100%"}
         className="mt-[15px] "
         onClick={submitHandler}
+        isLoading={loading}
       >
         Sign Up
       </Button>
